aa4fd5ed5f581169bc9274fe03cbac4e68fb7716
jhmoon <dothebestmoon1016@gmail.com>
2023-03-28 17:53:02 +0900

    DTO 14분부터 듣자

--- express-prac/src/controllers/users/index.js
+++ express-prac/src/controllers/users/index.js
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { UserDTO } from "./dto";
 
 // Router
 class UserController {
@@ -7,7 +8,8 @@ class UserController {
 	users = [
 		{
 			id: 1,
-			name : "moon",
+			firstName: "John",
+			lastName: "Don",
 			age : 12,
 		},
 	];
@@ -25,6 +27,7 @@ class UserController {
 
 	getUsers(req, res, next) {
 		try {
+			const users = this.users.map((user) => new UserDTO(user))
 			res.status(200).json({ users: this.users })
 		} catch (err) {
 			next(err);
@@ -34,10 +37,11 @@ class UserController {
 	getUser(req, res, next) {
 		try {
 			const { id } = req.params;
-			const user = this.users.find((user) => user.id === Number(id))
-			if (!user) {
+			const targetUser = this.users.find((user) => user.id === Number(id))
+			if (!targetUser) {
 				throw { status: 404, message: "유저를 찾을 수 없습니다." }
 			}
+			const user = new UserDTO(targetUser)
 
 			res.status(200).json({ user })
 		} catch (err) {
@@ -45,17 +49,27 @@ class UserController {
 		}
 	}
 
+	getUserFullName (req, res, next) {
+		try {
+
+		} catch (err) {
+			next(err)
+		}
+	}
+
 	createUser(req, res, next) {
 		try {
-			const { name, age } = req.body;
-			if (!name) {
+			const { firstName, lastName, age } = req.body;
+			if (!firstName || !lastName) {
 				throw { status: 400, message: "이름이 없습니다."}
 			}
 
 			this.users.push({
 				id: new Date().getTime(),
 				name,
-				age
+				age,
+				firstName,
+				lastName,
 			});
 			res.status(201).json({ users: this.user })
 		} catch (err) {
